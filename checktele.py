51de13242e397995311b1443991b24af1cbacc0e
AHMAD5Dd <105465818+AHMAD5Dd@users.noreply.github.com>
2023-02-22 06:59:41 +0300

    Create checktele.py

---
+++ checktele.py
@@ -0,0 +1,216 @@
+import random
+import threading
+import asyncio
+import telethon
+from telethon import events
+from queue import Queue
+import requests
+from telethon.sync import functions
+from user_agent import generate_user_agent
+import requests
+from user_agent import *
+from help import *
+from config import *
+from threading import Thread
+
+a = 'qwertyuiopassdfghjklzxcvbnm'
+b = '1234567890'
+e = 'qwertyuiopassdfghjklzxcvbnm1234567890'
+
+banned = []
+isclaim = ["off"]
+isauto =
+with open("banned.txt", "r") as f
+    f = f.read().split
+    banned.append(f)
+
+que = Queue()
+
+
+def check_user(
+    url = "https://t.me/"+str(
+    headers 
+        "User-Agent": 
+        "Accept": 
+        "Accept-Encoding": "gzip, deflate, br"
+        "Accept-Language": "ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7"}
+
+    response = requests.get(url, headers=headers
+    if response.text.find('If you have <strong>Telegram</strong>, you can contact <a class="tgme_username_link"') >= 0
+        return 
+    else
+        return "Unavailable"
+        
+
+def gen_user(choice
+    if choice == "1"
+        c = random.choices(a
+        d = random.choices(a
+        s = random.choices(e
+        f = [c[0], "_", d[0], "_", s[0
+        username = ''.join(f
+        if username in banned[0
+            c = random.choices(a
+            d = random.choices(b
+            s = random.choices(e
+            f = [c[0], "_", d[0], "_", s[0
+            username = ''.join(f
+        else
+            pass
+    if choice == "2":
+        c = random.choices(a)
+        d = random.choices(b)
+        s = random.choices(e)
+        f = [c[0], s[0], d[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        username = username+'bot'
+        if username in banned[0]:
+            c = random.choices(a)
+            d = random.choices(b)
+            s = random.choices(e)
+            f = [c[0], s[0], d[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+            username = username+'bot'
+        else:
+            pass
+    if choice == "3":
+        c = d = random.choices(a)
+        d = random.choices(b)
+        f = [c[0], d[0], c[0], c[0], d[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(b)
+            f = [c[0], d[0], c[0], c[0], d[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+        else:
+            pass
+    if choice == "4":
+        c = d = random.choices(a)
+        d = random.choices(b)
+        f = [c[0], d[0], c[0], c[0], c[0], d[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(b)
+            f = [c[0], d[0], c[0], c[0], c[0], d[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+        else:
+            pass
+    if choice == "5":
+        c = d = random.choices(a)
+        d = random.choices(b)
+        f = [c[0], c[0], c[0], c[0], d[0], c[0], c[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(b)
+            f = [c[0], c[0], c[0], c[0], d[0], c[0], c[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+        else:
+            pass
+    if choice == "6":
+        c = d = random.choices(a)
+        d = random.choices(b)
+        f = [c[0], d[0], "_", c[0], c[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(b)
+            f = [c[0], d[0], "_", c[0], c[0]]
+            random.shuffle(f)
+            username = ''.join(f)
+        else:
+            pass
+                if choice == "7":
+        c = d = random.choices(a)
+        d = random.choices(b)
+        f = [c[0], d[0], q[0], c[0]]
+        random.shuffle(f)
+        username = ''.join(f)
+        if username in banned[0]:
+            c = d = random.choices(a)
+            d = random.choices(b)
+            f = [c[0], d[0], "_", c[0], c[0]]
+            random.shuffle(f)
+            username = ''.join(f
+    return username
+    
+@fifthon.on(events.NewMessage(outgoing=True, pattern=r"\.الانواع"))
+async def _(event):
+    if ispay2[0] == "yes":
+        await event.edit(tele_checker2)
+# صيد عدد نوع قناة
+
+
+@bhthon.on(events.NewMessage(outgoing=True, pattern=r"\.صيد (.*)"))
+async def _(event):
+    if ispay2[0] == "yes":
+        isclaim.clear()
+        isclaim.append("on")
+        msg = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 2)
+        ch = str(msg[2])
+        choice = str(msg[1])
+        trys = 0
+await fifthon.send_message(event.chat_id, f"حسناً سأفحص نوع `{choice}` من اليوزرات على `{ch}` , بعدد `{msg[0]}` من المحاولات !")
+
+@Bhthon.on(events.NewMessage(outgoing=True, pattern=r"\.حالة الصيد"))
+        async def _(event):
+            if ispay2[0] == "yes":
+                if "on" in isclaim:
+                    await bhthon.send_message(event.chat_id, f"الصيد وصل لـ({trys}) من المحاولات")
+                elif "off" in isclaim:
+                    await event.edit("لايوجد صيد شغال !")
+                else:
+                    await event.edit("خطأ")
+            else:
+                pass
+        for i in range(int(msg[0])):
+            if ispay2[0] == 'no':
+                break
+            username = ""
+
+            username = gen_user(choice)
+            t = Thread(target=lambda q, arg1: q.put(
+                check_user(arg1)), args=(que, username))
+            t.start()
+            t.join()
+            isav = que.get()
+            if "Available" in isav:
+                await asyncio.sleep(1)
+                try:
+                    await fifthon(functions.channels.UpdateUsernameRequest(
+                        channel=ch, username=username))
+                    await event.client.send_message(event.chat_id, f'''
+✪ (@{username}) done...
+✪ By @BHthon - @myAbnBashar
+    ''')
+                    break
+                except telethon.errors.rpcerrorlist.UsernameInvalidError:
+                    with open("banned.txt", "a") as f:
+                        f.write(f"\n{username}")
+                except Exception as eee:
+                    await fifthon.send_message(event.chat_id, f'''خطأ مع {username}
+    الخطأ :
+    {str(eee)}''')
+                    if "A wait of" in str(eee):
+                        break
+                    else:
+                        await fifthon.send_message(event.chat.id, " خاصية او مزاد ")
+            else:
+                pass
+            trys += 1
+
+        isclaim.clear()
+        isclaim.append("off")
+        trys = ""
+        await event.client.send_message(event.chat_id, "!تم الانتهاء من الفحص")
